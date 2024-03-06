e4f7641beed71048995ada386e41380b6b81707e
Mbabazi-Hawa-Bint-Edrisa <143553200+Mbabazi-Hawa-Bint-Edrisa@users.noreply.github.com>
2024-03-06 23:14:12 +0600

    assignment_three

---
+++ objects/object_three.js
@@ -0,0 +1,11 @@
+// Create an object called person with name = John, age = 50. Then, access the object to alert("John is 50").
+
+const person = {
+  name: "John",
+  age: 50
+};
+
+const name = person.name;
+const age = person.age;
+
+alert(name + " is " + age);
